e8cb2d06e8f689bd843b8f1f3b303b52e07e7252
Артём Рыжов <ar.ryqp@mail.ru>
2024-12-01 00:51:39 +0300

    render template

--- flask_server.py
+++ flask_server.py
@@ -1,10 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_menu():
-    # response = send_command(command)
-    # print("Response:", response)
-    return "Hello bebra"
+    return render_template("index.html")
