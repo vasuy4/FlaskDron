0cac31408479806c62e3fbd7757da9a1082f4901
artem.ryzhov <artem.ryzhov@namu.ru>
2024-12-06 18:59:31 +0300

    del comment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask_server import app, auto_update
 from threading import Thread
 
 if __name__ == "__main__":
-    # thread_auto_update = Thread(target=auto_update)  # создание потока отправки буфера
-    # thread_auto_update.start()
+    thread_auto_update = Thread(target=auto_update)  # создание потока отправки буфера
+    thread_auto_update.start()
     app.run(debug=True)
-    # thread_auto_update.join()
+    thread_auto_update.join()
